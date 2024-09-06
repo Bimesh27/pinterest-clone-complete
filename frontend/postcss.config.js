1184972edfe504d84e2eec3d1719a0fc36407cf6
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-09-06 19:06:17 +0530

    ffffffffffffff

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,4 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// postcss.config.js
+module.exports = {
+   plugins: [require("tailwindcss"), require("autoprefixer")],
+};
