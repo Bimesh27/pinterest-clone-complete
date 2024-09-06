5700506cb30f0b67ae14f5cef7bb4118d99e4f83
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-09-06 18:56:00 +0530

    reinstall

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   server: {
-      proxy: {
-         "/api": {
-            target: "http://localhost:5000",
-            changeOrigin: true, // Ensures proper handling of CORS issues
-            // Optional: Log proxy errors
-            onError(err, req, res) {
-               console.log("Error in proxy:", err);
-               res.writeHead(500, {
-                  "Content-Type": "text/plain",
-               });
-               res.end("Something went wrong. Proxy error.");
-            },
-         },
-      },
-   },
-});
+  plugins: [react()],
+  server: {
+     proxy: {
+        '/api': 'http://localhost:5000'
+     }
+  }
+})
